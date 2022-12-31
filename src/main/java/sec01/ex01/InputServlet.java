bc19664dc5e2c9afd7294208cb771afb85f305e8
junsu <bobdylan0331@gmail.com>
2022-12-31 17:46:46 +0900

    commit self studying servlet

---
+++ src/main/java/sec01/ex01/InputServlet.java
@@ -0,0 +1,38 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InputServlet
+ */
+@WebServlet("/input")
+public class InputServlet extends HttpServlet {
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+	
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+	}
+
+	
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+	}
+}
